c75d75853d58ae49f5d05486f63b3335dcaaee24
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-10-31 11:30:43 +0100

    implement react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
-import App from './components/App';
+
+import Home from "./pages/Home/Home";
+import AddQuestion from "./pages/AddQuestion/AddQuestion";
+import Questions from "./pages/Questions/Questions";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/question",
+        children: [
+            {
+                path: "",
+                element: <Questions />,
+            },
+            {
+                path: "new",
+                element: <AddQuestion />,
+            }
+        ]
+    }
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
