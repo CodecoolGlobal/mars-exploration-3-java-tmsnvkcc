1cf28e7ec9facf317a2bf07d7e77f5ef2ff65cc5
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-10-31 14:35:55 +0100

    implement methods in application for ConsoleLogger and DB connection

--- backend/src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
+++ backend/src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
@@ -2,6 +2,10 @@ package com.codecool.stackoverflowtw;
 
 import com.codecool.stackoverflowtw.dao.QuestionsDAO;
 import com.codecool.stackoverflowtw.dao.QuestionsDaoJdbc;
+import com.codecool.stackoverflowtw.logger.ConsoleLogger;
+import com.codecool.stackoverflowtw.logger.Logger;
+import com.codecool.stackoverflowtw.sqlitedb.SqliteConnector;
+import com.codecool.stackoverflowtw.sqlitedb.SqliteConnectorImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +20,15 @@ public class StackoverflowTwApplication {
     @Bean
     public QuestionsDAO questionsDAO() {
         return new QuestionsDaoJdbc();
+    }
+
+    @Bean
+    Logger getLogger() {
+        return new ConsoleLogger();
+    }
+
+    @Bean
+    SqliteConnector getDBConnector() {
+        return new SqliteConnectorImpl(getLogger());
     }
 }
