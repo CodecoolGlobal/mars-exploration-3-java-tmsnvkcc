3e66d5ae22136dbb8c092e1445034f1b8032668d
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-10-31 11:32:00 +0100

    create and implement the home page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Home = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <h1>Home</h1>
+        </div>
+    );
+};
+
+export default Home;
