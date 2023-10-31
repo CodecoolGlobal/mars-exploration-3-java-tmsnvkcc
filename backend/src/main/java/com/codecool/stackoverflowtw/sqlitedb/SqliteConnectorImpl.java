81cabe89f050cb2f1b46b0299a88e7a681ac6ddf
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-10-31 14:34:41 +0100

    refactor SqliteConnectorImpl and its getConnection method

--- backend/src/main/java/com/codecool/stackoverflowtw/sqlitedb/SqliteConnectorImpl.java
+++ backend/src/main/java/com/codecool/stackoverflowtw/sqlitedb/SqliteConnectorImpl.java
@@ -5,23 +5,27 @@ import com.codecool.stackoverflowtw.logger.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class SqliteConnectorImpl implements SqliteConnector {
-    private final String connectionString;
     private final Logger logger;
 
-    public SqliteConnectorImpl(String connectionString, Logger logger) {
-        this.connectionString = connectionString;
+    public SqliteConnectorImpl(Logger logger) {
         this.logger = logger;
     }
 
     public Connection getConnection() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:" + connectionString);
-            logger.logInfo("Connection to SQLite has been established.");
+            Map<String, String> env = System.getenv();
+            String hostName = env.get("DB_HOST");
+            String port = env.get("DB_PORT");
+            String databaseName = env.get("DB_NAME");
 
-            return conn;
+            String connectionString = "jdbc:postgresql://" + hostName + ":" + port + "/"+ databaseName;
+            conn = DriverManager.getConnection(connectionString, env.get("DB_USERNAME"), env.get("DB_PASSWORD"));
+
+            logger.logInfo("Connection to postgresql has been established.");
         } catch (SQLException e) {
             logger.logError(e.getMessage());
         }
