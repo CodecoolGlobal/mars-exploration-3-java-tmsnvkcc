942e16a2773eb08af1d6e1f28c5acfc426628e28
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-10-31 11:32:49 +0100

    create and implement the page which will show every question

---
+++ frontend/src/pages/Questions/Questions.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+import "./Questions.css";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Questions = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <h1>Questions</h1>
+        </div>
+    );
+};
+
+export default Questions;
